d9f1b7168fab7f9d1881247b376af8fa37fda0d4
Onkar Yaglewad <121476278+once-human@users.noreply.github.com>
2024-08-12 02:03:43 +0530

    Favicon and small changes

--- src/templates/header.php
+++ src/templates/header.php
@@ -1,24 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
-    <link rel="stylesheet" href="../public/assets/css/header.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo isset($pageTitle) ? $pageTitle : "Synthetiq"; ?></title> <!-- Dynamic title -->
+    <link rel="stylesheet" href="../public/assets/css/header.css"> 
+    <link rel="icon" href="../public/assets/img/favicon.png" type="image/jpeg"> <!-- Favicon -->
 </head>
-<header>
-    <div class="logo">
-        <img src="../public/assets/img/navbar-logo.png" alt="site-Logo">
-    </div>
-    <nav>
-        <ul>
-            <li><a href="/public/index.php">Home</a></li>
-            <li><a href="/public/logout.php">Log out</a></li>
-            <li class="notification-icon icons">
-                <a href="#">
-                    <img src="../public/assets/img/notification.png" alt="Notification Icon" class="notification-img">
-                </a>
-                <div class="popup">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, deleniti.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, deleniti.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, deleniti.</p>
-                </div>
-            </li>
-        </ul>
-    </nav>
-</header>
+
+<body>
+    <header>
+        <div class="logo">
+            <img src="../public/assets/img/navbar-logo.png" alt="site-Logo">
+        </div>
+        <nav>
+            <ul>
+                <li><a href="../public/index.php">Home</a></li>
+                <li><a href="../public/logout.php">Log out</a></li>
+                <li class="notification-icon icons">
+                    <a href="#">
+                        <img src="../public/assets/img/notification.png" alt="Notification Icon" class="notification-img">
+                    </a>
+                    <div class="popup">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, deleniti.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, deleniti.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, deleniti.</p>
+                    </div>
+                </li>
+            </ul>
+        </nav>
+    </header>
