7307624e401244f3fd591b2fd24b7f07716fad9a
Kiran Biradar <biradarkiran.official@gmail.com>
2024-08-12 17:20:34 +0530

    added job posting page

---
+++ src/templates/job-posting-card2.php
@@ -0,0 +1,17 @@
+    <div class="job-card">
+        <div class="job-info">
+            <h3>${jobTitle}</h3>
+            <p>${companyName}</p>
+            <p>Location: ${location}</p>
+            <p>Work Mode: ${workMode}</p>
+            <button class="details-button">Details</button>
+        </div>
+        <div class="job-actions">
+            <span class="edit-button">Edit</span>
+            <span class="delete-button">Delete</span>
+        </div>
+        <div class="response-count">
+            Responses: 0
+        </div>
+    </div>
+    
